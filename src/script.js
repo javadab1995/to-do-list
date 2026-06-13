8703db44bb5f0441e5b5139b4993c447109b6faf
javadab1995 <javadabedini064@gmail.com>
2026-06-13 11:09:49 +0430

    fix styling coniguration

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import "./style.css"
+
 const taskInput = document.querySelector("#taskInput");
 const deadlineInput = document.querySelector("#deadlineInput");
 const btnAddTask = document.querySelector("#btnAddTask");
